f1ac27778864fc91a4194af61521b7de10859c87
umisetokikaze <umisetokikaze@gmail.com>
2024-02-16 08:03:49 +0000

    add params

--- src/inference/generate.py
+++ src/inference/generate.py
@@ -13,20 +13,26 @@ from inference.utils import *
 from core import load_or_fail
 from train import WurstCoreC, WurstCoreB
 
-def setup_device():
-    return torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-def load_yaml_config(config_name, config_path='./configs/inference'):
+def load_config(config_name, config_path='./configs/inference'):
     config_file = os.path.join(config_path, f'{config_name}.yaml')
     with open(config_file, "r", encoding="utf-8") as file:
         return yaml.safe_load(file)
 
-def initialize_model(core_class, config, device, training=False):
+def initialize_model(core_class, config, device, training=False,Bmode=False):
     core = core_class(config_dict=config, device=device, training=training)
     extras = core.setup_extras_pre()
     models = core.setup_models(extras)
     models.generator.eval().requires_grad_(False)
-    return core, models, extras
+    
+    if Bmode:
+        models = WurstCoreB.Models(
+        **{**models.to_dict(), 'tokenizer': models.tokenizer, 'text_model': models.text_model}
+        )
+        models.generator.bfloat16().eval().requires_grad_(False)
+        print("STAGE B READY")
+        return core,models,extras
+    print("STAGE C READY")
+    return core,models,extras
 
 def determine_model_sizes(model_type):
     sizes = {
@@ -45,35 +51,61 @@ def setup_sampling_configs(extras, cfg=4, shift=2, timesteps=20, t_start=1.0):
         't_start': t_start
     })
 
-def generate_images(core, core_b, models, models_b, extras, extras_b, batch, stage_c_latent_shape, stage_b_latent_shape, device):
+def generate(core, core_b, models, models_b, extras, extras_b, caption,batch_size, stage_c_latent_shape, stage_b_latent_shape, device):
+    os.makedirs('output', exist_ok=True)
+
+    # PREPARE CONDITIONS
+    batch = {'captions': [caption] * batch_size}
     conditions = core.get_conditions(batch, models, extras, is_eval=True, is_unconditional=False, eval_image_embeds=False)
-    unconditions = core.get_conditions(batch, models, extras, is_eval=True, is_unconditional=True, eval_image_embeds=False)
+    unconditions = core.get_conditions(batch, models, extras, is_eval=True, is_unconditional=True, eval_image_embeds=False)    
     conditions_b = core_b.get_conditions(batch, models_b, extras_b, is_eval=True, is_unconditional=False)
     unconditions_b = core_b.get_conditions(batch, models_b, extras_b, is_eval=True, is_unconditional=True)
 
     with torch.no_grad(), torch.cuda.amp.autocast(dtype=torch.bfloat16):
-        sampled_c = core.diffuse_and_sample(models, extras, conditions, unconditions, stage_c_latent_shape, device)
-        sampled_b = core_b.diffuse_and_sample(models_b, extras_b, conditions_b, unconditions_b, stage_b_latent_shape, device, effnet=sampled_c)
-    return sampled_b
+    # torch.manual_seed(42)
+        sampling_c = extras.gdf.sample(
+            models.generator, conditions, stage_c_latent_shape,
+            unconditions, device=device, **extras.sampling_configs,
+        )   
+        for (sampled_c, _, _) in tqdm(sampling_c, total=extras.sampling_configs['timesteps']):
+            sampled_c = sampled_c
+            
+        # preview_c = models.previewer(sampled_c).float()
+        # show_images(preview_c)
 
-def save_images(images, output_dir='output'):
-    os.makedirs(output_dir, exist_ok=True)
-    times = time.strftime(r"%Y%m%d%H%M%S")
-    file_names = []
-    for i, img in enumerate(images):
-        img_path = os.path.join(output_dir, f"img-{times}-{i+1}.png")
-        torchvision.transforms.functional.to_pil_image(img.clamp(0, 1)).save(img_path)
-        file_names.append(img_path)
-    return file_names
+        conditions_b['effnet'] = sampled_c
+        unconditions_b['effnet'] = torch.zeros_like(sampled_c)
 
-# Example usage:
-# device = setup_device()
-# config_c, config_b = load_yaml_config('your_config_c'), load_yaml_config('your_config_b')
-# core, models, extras = initialize_model(WurstCoreC, config_c, device)
-# core_b, models_b, extras_b = initialize_model(WurstCoreB, config_b, device)
-# model_sizes = determine_model_sizes("big-big")
-# setup_sampling_configs(extras)
-# setup_sampling_configs(extras_b, cfg=1.1, shift=1, timesteps=10)
-# batch = {'captions': ["your caption here"] * batch_size}
-# images = generate_images(core, core_b, models, models_b, extras, extras_b, batch, ...)
-# save_images(images)
+        sampling_b = extras_b.gdf.sample(
+            models_b.generator, conditions_b, stage_b_latent_shape,
+            unconditions_b, device=device, **extras_b.sampling_configs
+        )
+        for (sampled_b, _, _) in tqdm(sampling_b, total=extras_b.sampling_configs['timesteps']):
+            sampled_b = sampled_b
+        sampled = models_b.stage_a.decode(sampled_b).float()
+        # save_image
+        times = time.strftime(r"%Y%m%d%H%M%S")
+        imgs = []
+        for i, img in enumerate(sampled):
+            img = torchvision.transforms.functional.to_pil_image(img.clamp(0, 1))
+            fileName = f"img-{times}-{i+1}.png"
+            img.save(f"output/{fileName}")
+            imgs.append(img)
+        return imgs
+
+def t2i(batch_size, caption, height, width, presion,model_size,essential,cfg_c,cfg_b,shift_c,shift_b,step_c,step_b):
+    device= torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    download_model(essential, model_size, presion)
+    c_model_size, b_model_size = determine_model_sizes(model_size)
+    config_c,config_b = f'stage_c_{c_model_size}_{presion}',f'stage_b_{b_model_size}_{presion}'
+    config_c, config_b = load_config(config_c), load_config(config_b)
+    
+    core, models, extras = initialize_model(WurstCoreC, config_c, device)
+    core_b, models_b, extras_b = initialize_model(WurstCoreB, config_b, device,False,True)
+    
+    setup_sampling_configs(extras,cfg=cfg_c, shift=shift_c, timesteps=step_c)
+    setup_sampling_configs(extras_b, cfg=cfg_b, shift=shift_b, timesteps=step_b)
+    
+    stage_c_latent_shape, stage_b_latent_shape = calculate_latent_sizes(height, width, batch_size=batch_size)
+    images = generate(core, core_b, models, models_b, extras, extras_b, caption,batch_size, stage_c_latent_shape, stage_b_latent_shape, device)
+    return images
--- src/webui.py
+++ src/webui.py
@@ -1,6 +1,7 @@
 import gradio as gr
+from inference.generate import t2i
 
-from inference.text2img import generate
+
 def webui():
     with gr.Blocks() as ui:
         with gr.Row():
@@ -13,10 +14,16 @@ def webui():
                 batch_size = gr.Slider(1, 10, 4,step=1, label="Batch Size")
                 height = gr.Slider(64, 2048, 1024,step=2, label="Height")
                 width = gr.Slider(64, 2048, 1024,step=2, label="Width")
+                cfg_c = gr.Slider(1,20,4,step=0.1,label="cfg_c")
+                cfg_b = gr.Slider(1,20,1.1,step=0.1,label="cfg_b")
+                shift_c = gr.Slider(1,7,1,step=1,label="shift_c")
+                shift_b = gr.Slider(1,7,2,step=1,label="shift_b")
+                step_c = gr.Slider(1,200,20,step=2,label="step_c")
+                step_b = gr.Slider(1,200,10,step=2,label="step_b")
             with gr.Column():
                 output =gr.Gallery(label="Output Image")
                 run = gr.Button(value="Run")
-            run.click(fn=generate, inputs=[batch_size, caption, height, width, precision, model_size,essential], outputs=[output])
+            run.click(fn=t2i, inputs=[batch_size, caption, height, width, precision, model_size,essential,cfg_c,cfg_b,shift_c,shift_b,step_c,step_b], outputs=[output])
             ui.launch()
 
 webui()
