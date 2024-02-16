a90bca586fe1d5d4ecdfdd1241b8e20dfda0b572
Umisetokikaze <52318966+umisetokikaze@users.noreply.github.com>
2024-02-16 13:47:30 +0900

    webui.bat を更新

--- webui.bat
+++ webui.bat
@@ -1,26 +1,75 @@
 @echo off
-setlocal enabledelayedexpansion
-set VENV_DIR=venv
 
-REM 仮想環境が存在しない場合は作成
-if not exist "%VENV_DIR%" (
-		python -m venv %VENV_DIR%
-		echo 仮想環境を作成しました。
+if not defined PYTHON (set PYTHON=python)
+if not defined VENV_DIR (set "VENV_DIR=%~dp0%venv")
+
+set ERROR_REPORTING=FALSE
+mkdir tmp 2>NUL
+
+%PYTHON% -c "" >tmp/stdout.txt 2>tmp/stderr.txt
+if %ERRORLEVEL% == 0 goto :check_pip
+echo Couldn't launch python
+goto :show_stdout_stderr
+
+:check_pip
+%PYTHON% -mpip --help >tmp/stdout.txt 2>tmp/stderr.txt
+if %ERRORLEVEL% == 0 goto :start_venv
+if "%PIP_INSTALLER_LOCATION%" == "" goto :show_stdout_stderr
+%PYTHON% "%PIP_INSTALLER_LOCATION%" >tmp/stdout.txt 2>tmp/stderr.txt
+if %ERRORLEVEL% == 0 goto :start_venv
+echo Couldn't install pip
+goto :show_stdout_stderr
+
+:start_venv
+dir "%VENV_DIR%\Scripts\Python.exe" >tmp/stdout.txt 2>tmp/stderr.txt
+if %ERRORLEVEL% == 0 goto :activate_venv
+
+for /f "delims=" %%i in ('CALL %PYTHON% -c "import sys; print(sys.executable)"') do set PYTHON_FULLNAME="%%i"
+echo Creating venv in directory %VENV_DIR% using python %PYTHON_FULLNAME%
+%PYTHON_FULLNAME% -m venv "%VENV_DIR%" >tmp/stdout.txt 2>tmp/stderr.txt
+if %ERRORLEVEL% == 0 goto :activate_venv
+
+echo Unable to create venv in directory "%VENV_DIR%"
+goto :show_stdout_stderr
+
+:activate_venv
+set PYTHON="%VENV_DIR%\Scripts\Python.exe"
+CALL %VENV_DIR%\Scripts\activate
+echo venv %PYTHON%
+
+if exist "%VENV_DIR%\share\" (
+  goto :launch
+) else (
+pip install -r requirements.txt
 )
 
+:launch
+%PYTHON% ./src/webui.py %*
+pause
+exit /b
 
-REM 仮想環境をアクティベート
-set PYTHON=".\%VENV_DIR%\Scripts\activate"
 
-REM requirements.txt から必要なモジュールをインストール
-for /F "tokens=*" %%a in (requirements.txt) do (
-		pip show %%a > NUL
-		if errorlevel 1 (
-		start /b pip install %%a
-	)
-)
 
-REM srcディレクトリ内のwebui.pyを実行
-python src\webui.py
+:show_stdout_stderr
 
+echo.
+echo exit code: %errorlevel%
+
+for /f %%i in ("tmp\stdout.txt") do set size=%%~zi
+if %size% equ 0 goto :show_stderr
+echo.
+echo stdout:
+type tmp\stdout.txt
+
+:show_stderr
+for /f %%i in ("tmp\stderr.txt") do set size=%%~zi
+if %size% equ 0 goto :show_stderr
+echo.
+echo stderr:
+type tmp\stderr.txt
+
+:endofscript
+
+echo.
+echo Launch unsuccessful. Exiting.
 pause
