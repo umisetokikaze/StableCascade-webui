b45a60161ae01b0eede3e85d0d0e7c4e02691c31
Umisetokikaze <52318966+umisetokikaze@users.noreply.github.com>
2024-02-17 19:47:47 +0900

    webui.sh を更新

--- webui.sh
+++ webui.sh
@@ -5,18 +5,15 @@ VENV_DIR="venv"
 
 # 仮想環境が存在しない場合は作成
 if [ ! -d "$VENV_DIR" ]; then
-    python -m venv venv
+    python -m venv $VENV_DIR
     echo "仮想環境を作成しました。"
 fi
 
 # 仮想環境をアクティベート
-. ./$VENV_DIR/bin/activate
+source ./$VENV_DIR/bin/activate
 
 # requirements.txt から必要なモジュールをインストール
-while read requirement; do
-    pip show "${requirement%%=*}" > /dev/null || pip install "${requirement%%=*}" &
-done < requirements.txt
-wait
+pip install -r requirements.txt
 
 # srcディレクトリ内のwebui.pyを実行
-python src/webui.py
+python src/webui.py "$@"
